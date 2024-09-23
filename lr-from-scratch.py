546513b83964277dc7ab1bfa1865a9c420823503
Enoch <49845680+enoch-xxiv@users.noreply.github.com>
2024-09-23 07:49:34 +0100

    start exploratory data analysis

--- lr-from-scratch.py
+++ lr-from-scratch.py
@@ -4,3 +4,7 @@ import matplotlib as plt
 
 data = pd.read_csv('./sales-data.csv')
 print(data)
+
+# We assume that this data set can be modelled linearly.
+
+# Function to get prediction given a model and an x value.
